321311bb891ffee98babe60e3396ba7be7695ac1
Alex <barajasr@sonoma.edu>
2020-03-18 12:11:12 -0700

    Im confused

--- 415project2.py
+++ 415project2.py
@@ -9,15 +9,33 @@ class Client:
         print(self.end)
         print(self.value)
 
+    def getEnd(self):
+        return self.end
+
+    def getStart(self):
+        return self.start
+
+    def getvalue(self):
+        return self.value
 
 class DAG:
-    def __init__(self):
-        pass
+    # pass a list of client objects to create the DAG ?
+    def __init__(self, listOfClient):
+        self.clients = listOfClient
+        self.graph = {}
 
+    def buildgraph(self):
+        # double for loops to check every client
+        for i in self.clients:
+            for j in self.clients:
+                if self.clients[i].getEnd <= self.clients[j].getStart:
+                    print("found")
 def main():
     c1 = Client(1, 2, 5)
     c2 = Client(2, 3, 5)
     c3 = Client(3, 4, 5)
+    list = [c1, c2, c3]
+    graph = DAG(list)
     c1.descrition()
     c2.descrition()
     c3.descrition()
