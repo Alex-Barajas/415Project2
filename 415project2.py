92159238649d3e6725f4bfc8ed684acbd19c79fe
Alex <barajasr@sonoma.edu>
2020-03-19 20:49:09 -0700

    Progress on Top

--- 415project2.py
+++ 415project2.py
@@ -33,20 +33,25 @@ class DAG:
     def __init__(self, list):
         self.clientlist = list
         self.clientNames = []
+        self.startClients = []
+        self.edges = {}
         for elem in list:
             self.clientNames.append(elem.name)
+            self.edges[elem.name] = 0
         self.graph = {}
         self.buildgraph()
+
 
     def buildgraph(self):
         # double for loops to check every client
         start = self.clientNames
         for elem in self.clientlist:
             for other in self.clientlist:
-                if elem.end <= other.start:
+                if int(elem.end) <= int(other.start) and elem.name != other.name:
                     if elem.name not in self.graph.keys():
                         self.graph[elem.name] = []
                     self.graph[elem.name].append(other.name)
+                    self.edges[other.name] += 1
                     if other.name in start:
                         start.remove(other.name)
                         
@@ -54,64 +59,47 @@ class DAG:
                 self.graph[elem.name] = []
                 self.graph[elem.name].append('End')
         self.graph['Start'] = start
+        self.startClients = start
 
     def description(self):
-        print(self.graph)
-        # pprint.pprint(self.graph)
+        #print(self.graph)
+        pprint.pprint(self.graph)
 
-#class topGraph: # we might not need this
-        #def __init__(self, vertices):
-        #self.V = vertices
-        #self.graph = defaultdict() # how can we use the graph we already created ?
+    def dict(self):
+        return self.graph
+
 
 def top_sort(graph):
-    queue = []
+    queue = graph.startClients
     result_top = []
+    templist = []
+    dict = graph.dict()
+    incomingEdges = graph.edges
 
-    # first create graph and generate each in-degree
-    for u in graph:
-        in_degree_count[u] = 0
-
-    #in_degree_count = {u: 0 for u in graph}
-    for u in graph:
-        for v in graph[u]: # process every edge
-            in_degree_count[v] += 1
-
-    # queue all nodes with indegree =0
-    for i in in_degree_count:
-        if in_degree_count[i] == 0:
-            queue.append(i)
-
-    visited_count = 0 # visited vertices might not need this
-    # store the topo sorted
     while queue:
-        ver = queue.pop(0)
-        result_top.append(ver) # dequeue and append to topo sorted
-
-        for neighbor in graph[ver]: # go thru all neighbors nodes of ver, decrease their in-degree by 1
-            in_degree_count[neighbor] -= 1
-            if in_degree_count[neighbor] == 0:
-                queue.append(neighbor)
-
-
-        visited_count +=1
+        ver = queue.pop(0) # A
+        verNeighnors = dict[ver]
+        for client in verNeighnors:
+            if client != 'End':
+                incomingEdges[client] -= 1
+                queue.append(client)
+        result_top.append(ver)  # dequeue and append to topo sorted
     return result_top
-
-
 
 
 def main():
     filename = input('En†er file name ')
     file = open(filename)
-    clientId = 0
+    clientId = 'A'
     listofClients = []
     for line in file:
         clientData = line.split()
         newClient = Client(clientId, clientData[0],  clientData[1],  clientData[2])
-        # Clientname = chr(ord(Clientname) + 1)
-        clientId += 1
+        clientId = chr(ord(clientId) + 1)
+        # clientId += 1
         listofClients.append(newClient)
     graph = DAG(listofClients)
+    top_sort(graph)
     graph.description()
 
 
